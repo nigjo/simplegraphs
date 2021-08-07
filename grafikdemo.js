142ab5afdd4777ddfbbdbb1eea7020c9a7bfe1cf
Jens Hofschröer <git@nigjo.de>
2021-08-07 17:22:56 +0200

    initialer Stand mit verschiedenen Grafiken

---
+++ grafikdemo.js
@@ -0,0 +1,129 @@
+var fn=[];
+var lastBild = null;
+const PREFIX = "bild_";
+
+window.addEventListener('load', ()=>{
+
+  fn = Object.getOwnPropertyNames(window)
+    .filter(item => item.startsWith(PREFIX))
+    .filter(item => typeof window[item] === 'function')
+    .map(name=>window[name]);
+
+  updateList();
+
+  nextBild();
+});
+
+function updateList(){
+  let orglist = document.getElementById("liste");
+  list = orglist.cloneNode();
+  
+  let sorted = fn.sort((a,b)=>a.name.localeCompare(b.name));
+  for(let n of sorted){
+    let item = document.createElement("LI");
+    let link = document.createElement("A");
+    
+    link.href="#";
+    let bildFn = n;
+    link.onclick=(e)=>{
+      e.preventDefault();
+      nextBild(bildFn);
+      return false;
+    };
+    link.textContent = bildFn.name.substring(PREFIX.length);
+    
+    list.appendChild(item).appendChild(link);
+  }
+  
+  orglist.replaceWith(list);
+}
+
+function nextBild(forced=null){
+  let next;
+  if(forced){
+    next = forced;
+  }else if(fn.length>0){
+    do{
+      next = fn[parseInt(Math.random()*fn.length,10)];
+    }
+    while(next==lastBild);
+  }else{
+    let nothing_found=()=>{};
+    next=nothing_found;
+  }
+  lastBild = next;
+  console.log("next:",lastBild.name);
+  
+  document.getElementById("bild")
+    .replaceWith(document.getElementById("bild").cloneNode(true));
+  
+  _animate(lastBild);
+}
+
+function _paint_debug(bildFn){
+
+  document.querySelector("figcaption").textContent = bildFn.name;
+
+  var bild = document.getElementById("bild");
+  const ctx = bild.getContext('2d');
+  //ctx.fillStyle = 'lightgreen';
+  ctx.clearRect(0, 0, bild.width, bild.height);
+  ctx.beginPath();
+  bildFn(bild,ctx);
+  ctx.stroke();
+  
+  fn=[bildFn];
+  lastBild = null;
+  
+  _animate = _paint
+}
+
+var animator=null;
+
+function _animate(bildFn){
+  var bild = document.getElementById("bild");
+  const ctx = bild.getContext('2d');
+  
+  var frame = 0;
+  frame = 0;
+  var count;
+  count = 0;
+  
+  var orgLine = ctx.lineTo;
+  ctx.lineTo = function(x,y) {
+    if(0<count--)
+      orgLine.call(ctx,x,y);
+  };
+  
+  ctx.clearRect(0,0,bild.width, bild.height);
+  
+  framePainter = function()
+  {
+    count = ++frame;
+    
+    //ctx.fillStyle = 'lightgreen';
+    //ctx.fillRect(0, 0, bild.width, bild.height);
+    ctx.clearRect(0, 0, bild.width, bild.height);
+    
+    ctx.beginPath();
+    bildFn(bild,ctx);
+    ctx.stroke();
+    
+    if(count>0){
+      window.clearInterval(animator);
+      document.body.className = "done";
+      console.log("count",count,"frame",frame);
+    }
+  }
+  
+  document.querySelector("figcaption").textContent =
+      bildFn.name.substring(PREFIX.length);
+  document.body.className = "working";
+  
+  window.clearInterval(animator);
+
+  animator = window.setInterval(framePainter, 75);
+  //window.requestAnimationFrame(framePainter);
+
+  //demo4(bild,ctx);
+};
---
+++ grafiken.js
@@ -0,0 +1,163 @@
+function bild_spirale(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  const count=20;
+  let dw = w/2/count;
+  let dh = h/2/count;
+  
+  ctx.moveTo(w/2,h/2);
+  for(i=count;i>=0;i--){
+    ctx.lineTo(w/2,i*dh);
+    ctx.lineTo(w-i*dw,i*dh);
+    ctx.lineTo(w-i*dw,h/2);
+    ctx.lineTo(w-i*dw,h-i*dh);
+    ctx.lineTo(w/2,h-i*dh);
+    ctx.lineTo(i*dw,h-i*dh);
+    ctx.lineTo(i*dw,h/2);
+    ctx.lineTo(i*dw,(i-1)*dh);
+  }
+};
+
+function bild_sanduhr(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  let d = w/50;
+
+  for(var i=0;i<=w/d;i++){
+    ctx.moveTo(i*d,0);
+    ctx.lineTo(w-i*d,h);
+  }
+};
+
+function bild_langer_gang(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  let dw = w/50;
+  let dh = h/30;
+
+  for(var i=0;i<=w/dw;i++){
+    ctx.moveTo(i*dw,h);
+    ctx.lineTo(w-i*dw,0);
+  }
+  for(var i=1;i<h/dh;i++){
+    ctx.moveTo(0,i*dh);
+    ctx.lineTo(w,h-i*dh);
+  }
+};
+
+function bild_katzenauge(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  
+  for(var i=0;i<40;i++){
+    ctx.moveTo(0,h-i*h/40);
+    ctx.lineTo(i*w/20,0);
+  }
+  for(var i=0;i<40;i++){
+    ctx.moveTo(w,i*h/40);
+    ctx.lineTo(w-i*w/20,h);
+  }
+}
+
+function bild_auge(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  const steps=30;
+  
+  for(var i=0;i<=steps;i++){
+    ctx.moveTo(0,h-i*h/steps);
+    ctx.lineTo(i*w/steps,0);
+  }
+  for(var i=1;i<=steps;i++){
+    ctx.moveTo(i*w/steps,0);
+    ctx.lineTo(w,i*h/steps);
+  }
+  for(var i=1;i<=steps;i++){
+    ctx.moveTo(w,i*h/steps);
+    ctx.lineTo(w-i*w/steps,h);
+  }
+  for(var i=1;i<=steps;i++){
+    ctx.moveTo(w-i*w/steps,h);
+    ctx.lineTo(0,h-i*h/steps);
+  }
+}
+
+function bild_kreis(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  const mx=w/2;
+  const my=h/2;
+  const l=Math.min(mx,my);
+
+  for(var i=0;i<360;i+=5){
+    ctx.moveTo(mx,my);
+    let x=mx+l*Math.cos(i/180*Math.PI);
+    let y=my+l*Math.sin(i/180*Math.PI);
+    ctx.lineTo(x,y);
+    ctx.lineTo(x<=mx?0:w,y);
+  }
+}
+
+function bild_Flusstal(bild,ctx){
+  const w = bild.width;
+  const h = bild.height;
+  for(var i=0;i<w/4;i+=10){
+    ctx.moveTo(0,0);
+    ctx.lineTo(i,h);
+  }
+  for(var i=0;i<w;i+=10){
+    ctx.moveTo(w/4+i/2,h);
+    ctx.lineTo(i,0);
+  }
+  for(var i=w/4;i>=0;i-=10){
+    ctx.moveTo(w,0);
+    ctx.lineTo(w-i,h);
+  }
+}
+
+function bild_doppelfaecher(bild,ctx){
+  for(var i=0;i<=bild.width;i+=12){
+    ctx.moveTo(0,bild.height);
+    ctx.lineTo(i,0);
+  }
+
+  for(var i=0;i<=bild.width;i+=12){
+    ctx.moveTo(i,bild.height);
+    ctx.lineTo(bild.width,0);
+  }
+}
+
+function bild_Saeulen(bild,ctx){
+  let w=bild.width;
+  let h=bild.height;
+  const count=5;
+  const steps=25;
+  
+  let dc = w/count;
+  let ds = dc/steps;
+  
+  for(let i=0;i<count;i++){
+    for(let s=1;s<=steps;s++){
+      let x = (dc/2)*Math.cos(s*Math.PI/steps)
+      ctx.moveTo(i*dc+dc-x-dc/2,0);
+      ctx.lineTo(i*dc+dc-x-dc/2,h);
+    }
+  }
+}
+
+
+function bild_Raute(bild,ctx){
+  let w = bild.width/2;
+  let h = bild.height/2;
+  const steps = 20;
+  let dw = w/steps;
+  let dh = h/steps;
+  for(let i=0;i<=steps;i++){
+    ctx.moveTo(i*dw,h+i*dh);
+    ctx.lineTo(w+i*dw,i*dh);
+  }
+  for(let i=0;i<=steps;i++){
+    ctx.moveTo(i*dw,h-i*dh);
+    ctx.lineTo(w+i*dw,2*h-i*dh);
+  }
+}
